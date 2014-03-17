5b8b14cb11a94341b0907d95c2ee7b1ba46a0b93
Nick Shvelidze <captain@pirrate.me>
2014-03-17 20:31:14 +0400

    Simple websocket server

---
+++ irl.js
@@ -0,0 +1,16 @@
+var WebSocketServer = require('ws').Server
+  , wss = new WebSocketServer({port: 8080});
+
+wss.broadcast = function(data) {
+    for(var i in this.clients)
+        this.clients[i].send(data);
+};
+
+wss.on('connection', function(ws) {
+    ws.on('message', function(message) {
+        console.log('received: %s', message);
+        wss.broadcast(message);
+    });
+    console.log("client connected");
+    wss.broadcast('New client connected');
+});
