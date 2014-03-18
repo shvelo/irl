8bf8f2e2eef7ecc6297e54634077181a3ca02997
Nick Shvelidze <captain@pirrate.me>
2014-03-18 15:27:27 +0400

    Add UUIDs

--- irl.js
+++ irl.js
@@ -1,5 +1,8 @@
-var WebSocketServer = require('ws').Server
-  , wss = new WebSocketServer({port: 8080});
+var WebSocketServer = require('ws').Server,
+    uuid = require('node-uuid');
+
+
+var wss = new WebSocketServer({port: 8080});
 
 wss.broadcast = function(data) {
     for(var i in this.clients)
@@ -7,14 +10,18 @@ wss.broadcast = function(data) {
 };
 
 wss.on('connection', function(ws) {
+    var userid = uuid.v4();
+
     ws.on('message', function(message) {
-        console.log('received: %s', message);
+        console.log('received: %s from %s', message, userid);
         wss.broadcast(message);
     });
+
     ws.on('close', function(){
-        console.log("client disconnected");
-        wss.broadcast("Client disconnected");
+        console.log("client disconnected %s", userid);
+        wss.broadcast("Client disconnected "+ userid);
     })
-    console.log("client connected");
-    wss.broadcast('New client connected');
+
+    console.log("client connected %s", userid);
+    wss.broadcast('New client connected '+ userid);
 });
